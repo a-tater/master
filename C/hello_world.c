5655fc1cfac1f3ba8631038712cef6bfa7c207bd
a-tater <159712750+a-tater@users.noreply.github.com>
2024-02-11 15:29:10 -0500

    adding newline and comments

--- C/hello_world.c
+++ C/hello_world.c
@@ -4,3 +4,33 @@ int main() {
     printf("Hello World!");
     return 0;
 }
+
+//could also be written as:
+int main(){printf("Hello World!");return 0;}
+
+//C ignores white space
+
+//main() {} is function, executes all statements inside {}
+//each statement is separated by semi-colon
+//printf() requires double quotation marks " "
+//return 0 ends the C program successfully
+
+
+// \n inserts newline
+int main() {
+    printf("Hello World!");
+    printf(" This prints out on the same line");
+    printf("\nHello World!\n");
+    printf("This prints out on a new line");
+    return 0;
+}
+
+// \n = newline
+// \t = creates horizontal tab
+// \\ = inserts a backslash character (first backslash excapes second backslash as special character)
+// \" = inserts a double quote character (backslash escapes quotation mark as a special character)
+
+/* this is a 
+multiline comment
+*/
+
