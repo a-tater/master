82fe245ec03afe213af4af808a752e2b7c2e36a7
a-tater <159712750+a-tater@users.noreply.github.com>
2024-02-12 20:37:27 -0500

    adding operators

---
+++ C/operators.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main() {
+
+/*
+Arithmatic Operators
+Operator    Name        Description         Example     x = 5, y = 2
++           Add         Adds two values     x + y       = 7
+-           Subtact     Subtracts           x - y       = 3
+*           Multiply    Multiplies          x * y       = 10
+/           Division    Divides             x / y       = 2
+%           Modulus     Returns remainder   x % y       = 1
+++          Increment   Increases by 1      ++x         = 3
+--          Decriment   Decreases by 1      --x         = 1
+
+Assignment Operators
+Operator    Name        Description     Same As     x = 5
+=                       x = 5           x = 5       = 5
++=                      x += 3          x = x + 3   = 8
+-=                      x -= 3          x = x - 3   = 2
+*=                      x *= 3          x = x * 3   = 15
+/=                      x /= 3          x = x / 3   = 2
+%=                      x %= 3          x = x % 3   = 1
+&=         Bin AND      x &= 3          x = x & 3   = 0101 & 0011  = 0001 = 1
+|=         Bin OR       x |= 3          x = x | 3   = 0101 | 0011  = 0111 = 7
+^=         Bin XOR      x ^= 3          x = x ^ 3   = 0101 ^ 0011  = 0110 = 6
+>>=  Bin Right Shift    x >>= 3         x = x >> 3  = 0101 >> 3    = 0000 = 0
+<<=  Bin Left Shift     x <<= 3         x = x << 3  = 0101 << 3    = 0010 1000 = 40
+
+Comparison Operators
+Operator    Name                    Example    x = 5
+==          Equal to                x == 5     1 = True
+!=          Not Eaqual to           x != 5     0 = False
+>           Greater than            x > 6      0 = False
+<           Less that               x < 6      1 = True
+>=          Greater than or = to    x >= 6     0 = False
+<=          Less than or = to       x <= 5     1 = True
+
+Logical Operators
+Operator    Name            Example             x = 5
+&&          Logical AND     x < 5 && x < 10     0 = False
+||          Logical OR      x < 5 || x < 6      1 = True
+!           Logical NOT     !(x < 5 || x < 6)   0 = False
+
+
+
+
+*/
+
+
+
+    return 0;
+}
