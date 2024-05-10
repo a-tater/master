915624c2988fed23e46d18386eead64c20102e1c
a-tater <159712750+a-tater@users.noreply.github.com>
2024-05-10 18:21:01 -0400

    adding first program

--- Java/welcome.java
+++ Java/welcome.java
@@ -1,7 +1,10 @@
-public class Welcome {
+public class welcome { // class name and file name must be exactly the same
     public static void main(String[] args) {
         //Display message Welcome to Java! on the console
         System.out.println("Welcome to Java!"); // creates newline AFTER
         System.out.print("Welcome to Java!"); // does not create newline after
     }
 }
+
+// to compile javac filename.java  //extension required
+// to run java filename  //no extension required
