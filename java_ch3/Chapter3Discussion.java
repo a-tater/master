24d421b53eaae7d3889430692f24d239f60742e7
a-tater <159712750+a-tater@users.noreply.github.com>
2024-06-09 14:42:25 -0400

    .

--- java_ch3/Chapter3Discussion.java
+++ java_ch3/Chapter3Discussion.java
@@ -9,17 +9,17 @@ public class Chapter3Discussion {
 
     /*  nested if, multi-way if , switch statement*/
 
-    if (yourNum < 5) {
-        if (yourNum < 2) // this is a basic if statement
+    if (yourNum < 5) { // this is a nested if
+        if (yourNum < 2) // the nested if is wrapped in brackets and indented on the next line
           System.out.println("Your number is the number 1!");
-    }
-    else if (yourNum < 50) {
-      switch (yourNum) {
-        case 10: System.out.println("Your number is ten!"); break;
-        case 20: System.out.println("Your number is twenty!"); break;
-        case 35: System.out.println("Your number is thirty!"); break;
-        default: System.out.println("Your number is not ten, twenty, or thirty, but it's still less than fifty!");
-          System.exit(1);
+    } // this bracket closes out the first if statement
+    else if (yourNum < 50) { // for else if, drop a line, include the boolean test, and open another set of brackets
+      switch (yourNum) { // this switch statement is nested in the else-if statement (includes a value to be tested against with case statments)
+        case 10: System.out.println("Your number is ten!"); break; // if a case stement evaluates to true, the code to the right of the : is executed
+        case 20: System.out.println("Your number is twenty!"); break; // in order to break out of the case statement, a break; is included after the code to be executed
+        case 30: System.out.println("Your number is thirty!"); break;
+        default: System.out.println("Your number is not ten, twenty, or thirty, but it's still less than fifty!"); // a default case runs in the same way an 'else' statement would execute
+          System.exit(1); // the System.exit(1) allows the code to exit gracefully following the case statement
       }
     }
   }
